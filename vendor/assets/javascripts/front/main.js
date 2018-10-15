7563d2b573536d076f8997c858470c25ad06293f
Anton Kostin <schnibba@gmail.com>
2018-10-15 18:19:36 +0300

    Новости полноценные, новая картинка. Подтверждение почты в Яндексе.

--- vendor/assets/javascripts/front/main.js
+++ vendor/assets/javascripts/front/main.js
@@ -1,145 +1,168 @@
 var userAgent = navigator.userAgent.toLowerCase(),
+
     isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
     isTouch = "ontouchstart" in window,
-    mobile = !1,
-    sliderBanner = null;
-(isMobile || isTouch) && (mobile = !0), $(document).ready(function() {
-    var e = ($(document), $(window));
-    $("html"), $("body"), $(window).width();
-    $("img, a, button").on("dragstart", function(e) {
-        e.preventDefault()
+
+    mobile = false;
+
+var sliderBanner = null;
+
+if(isMobile || isTouch)  {
+    mobile = true;
+}
+
+$(document).ready(function () {
+    var $document = $(document),
+        $window = $(window),
+        $html = $("html"),
+        $body = $("body");
+
+    var windowWidth = $(window).width();
+    
+    //Prevent dragging img, links, buttons
+    $("img, a, button").on("dragstart", function(event) { event.preventDefault(); });
+
+    //Object-fit
+    var $fitImages = $('img.js-fit');
+    $(function () { objectFitImages($fitImages, {watchMQ: true}); });
+    
+    //Menu
+    $('.js-menu-toggle').on('click', function(e) {
+        $('.js-menu').fadeToggle();
     });
-    var s = $("img.js-fit");
-    $(function() {
-        objectFitImages(s, {
-            watchMQ: !0
-        })
-    });
-    var t = $(".js-drops"),
-        a = t.length;
-    e.scroll(function() {
-        var e = $(window).scrollTop();
-        window.requestAnimationFrame(function() {
-            for (var s = 0; s < a; s++) {
-                var n = t.eq(s);
-                0 === s ? e < $(window).height() && n.css({
-                    transform: "translate3d(0," + e * -.25 + "px, 0)"
-                }) : n.css({
-                    transform: "translate3d(0," + (e - .98 * n.offset().top) * -.25 + "px, 0)"
-                })
-            }
-        })
-    });
-    var n = $(".scene"),
-        i = [],
-        o = {
-            relativeInput: !0,
-            clipRelativeInput: !1,
-            hoverOnly: !0,
-            calibrateX: !0,
-            calibrateY: !0,
-            invertX: !0,
-            invertY: !0,
-            limitX: 35,
-            limitY: 35,
-            frictionX: .2,
-            frictionY: .1,
-            selector: ".layer",
-            pointerEvents: !0
-        };
-    e.width() > 1023 && !isMobile && n.each(function(e) {
-        var s = $(".scene").eq(e);
-        s.hasClass("scene_min") ? (o.limitY = 20, o.limitX = 20, o.frictionX = .1, i[e] = new Parallax(n[e], o)) : s.hasClass("scene_horizontal") ? (o.limitY = 0, o.limitX = 50, o.frictionX = .1, i[e] = new Parallax(n[e], o)) : s.hasClass("scene_vertical") ? (o.limitX = 0, o.limitY = 50, o.frictionY = .1, i[e] = new Parallax(n[e], o)) : (o.limitY = 35, o.limitX = 35, o.frictionX = .2, i[e] = new Parallax(n[e], o))
-    }), jQuery(window).resize(function() {
-        $(".scene").each(function() {
-            $(window).width() < 1024 && $(this).attr("style", "")
-        }), $(".layer").each(function() {
-            $(window).width() < 1024 && $(this).attr("style", "")
-        })
-    }), $(".js-menu-toggle").on("click", function(e) {
-        $(".js-menu").fadeToggle()
-    }), jQuery(document).click(function(e) {
+
+    jQuery(document).click(function (e) {
         if ($(window).width() < 1023) {
-            var s = $(e.target).hasClass("js-menu") || $(e.target).closest(".header__menu").hasClass("js-menu"),
-                t = $(e.target).hasClass("js-menu-toggle") || $(e.target).closest(".header__toggle").hasClass("js-menu-toggle");
-            s || t || $(".js-menu").fadeOut()
+            var isMenu = $(e.target).hasClass('js-menu') || $(e.target).closest('.header__menu').hasClass('js-menu');
+            var isToggle = $(e.target).hasClass('js-menu-toggle') || $(e.target).closest('.header__toggle').hasClass('js-menu-toggle');
+
+            if (!isMenu && !isToggle) {
+                $('.js-menu').fadeOut();
+            } 
         }
-    }), $(".js-menu .js-anchor-link").on("click", function(e) {
-        $(window).width() < 1023 && $(".js-menu").fadeOut()
     });
-    var l = {},
-        r = {
-            loop: !1,
-            slidesPerView: 2,
-            threshold: 0,
-            spaceBetween: 6,
-            speed: 400,
-            slideClass: "about__gallery-item",
-            wrapperClass: "about__gallery-list",
-            navigation: {
-                nextEl: $(".js-slider-about-next"),
-                prevEl: $(".js-slider-about-prev")
+
+    $('.js-menu .js-anchor-link').on('click', function(e) {
+        if ($(window).width() < 1023) {
+            $('.js-menu').fadeOut();
+        }
+    });
+
+
+    //About slider init on mobile
+    var sliders = {};
+
+    var sliderSettings = {
+        loop: false,
+        slidesPerView: 2,
+        threshold: 0,
+        spaceBetween:6,
+        speed:400,
+        slideClass:"about__gallery-item",
+        wrapperClass:"about__gallery-list",
+        
+        navigation: {
+            nextEl: $('.js-slider-about-next'),
+            prevEl: $('.js-slider-about-prev')
+        },
+    };
+
+    $(".js-slider-about").each(function (i) {
+        sliders[i] = null;
+    });
+
+    $(".js-slider-about").each(function (i) {
+
+        if ($(window).width() < 768) {
+            if (sliders[i] === null && $(".js-slider-about").length > 0) {
+                sliders[i] = new Swiper ($('.js-slider-about').eq(i), sliderSettings);
             }
-        };
-    $(".js-slider-about").each(function(e) {
-        l[e] = null
-    }), $(".js-slider-about").each(function(e) {
-        $(window).width() < 768 ? null === l[e] && $(".js-slider-about").length > 0 && (l[e] = new Swiper($(".js-slider-about").eq(e), r)) : l[e] && (l[e].destroy(), l[e] = null, $(".about__gallery-list").removeAttr("style"))
-    }), jQuery(window).resize(function() {
-        $(".js-slider-about").each(function(e) {
-            $(window).width() < 768 ? null === l[e] && $(".js-slider-about").length > 0 && (l[e] = new Swiper($(".js-slider-about").eq(e), r)) : l[e] && (l[e].destroy(), l[e] = null, $(".about__gallery-list").removeAttr("style"))
-        })
+        } else {
+            if (sliders[i]) {
+                sliders[i].destroy();
+                sliders[i] = null;
+                $('.about__gallery-list').removeAttr('style');
+            }
+        }
     });
-    new Swiper(".js-slider-reviews", {
-        loop: !1,
+
+    jQuery(window).resize(function () {
+
+        $(".js-slider-about").each(function (i) {
+            if ($(window).width() < 768) {
+                if (sliders[i] === null && $(".js-slider-about").length > 0) {
+                    sliders[i] = new Swiper ($('.js-slider-about').eq(i), sliderSettings);
+                }
+            } else {
+                if (sliders[i]) {
+                    sliders[i].destroy();
+                    sliders[i] = null;
+                    $('.about__gallery-list').removeAttr('style');
+                }
+            }
+        });
+    });
+    
+
+    //Slider Reviews
+    var sliderReviews = new Swiper('.js-slider-reviews', {
+        loop: false,
         slidesPerView: 4,
-        spaceBetween: 4.522613065326633 * $(".js-slider-reviews .swiper-wrapper").width() / 100,
+        spaceBetween: ( $('.js-slider-reviews .swiper-wrapper').width()*4.522613065326633/100 ), // 60 /1194 full w
         speed: 600,
         threshold: 40,
+
         navigation: {
-            nextEl: $(".js-slider-reviews-next"),
-            prevEl: $(".js-slider-reviews-prev")
+            nextEl: $('.js-slider-reviews-next'),
+            prevEl: $('.js-slider-reviews-prev')
         },
+
         breakpoints: {
             767: {
                 threshold: 0,
                 slidesPerView: 1,
                 spaceBetween: 20,
-                autoHeight: !0
+                autoHeight: true,
             },
             1023: {
                 spaceBetween: 20,
-                slidesPerView: 2
+                slidesPerView: 2,
             },
             1419: {
                 spaceBetween: 20,
-                slidesPerView: 4
+                slidesPerView: 4,
             },
             1919: {
                 spaceBetween: 54,
-                slidesPerView: 3
+                slidesPerView: 3,
             }
         }
-    }), new Swiper(".js-slider-news", {
-        loop: !1,
+    });
+
+    //Slider News
+    var sliderNews = new Swiper('.js-slider-news', {
+        loop: false,
         slidesPerView: 2,
         slidesPerGroup: 2,
         spaceBetween: 60,
         speed: 600,
         threshold: 40,
-        autoHeight: !0,
+        autoHeight: true,
+
         navigation: {
-            nextEl: $(".js-slider-news-next"),
-            prevEl: $(".js-slider-news-prev")
+            nextEl: $('.js-slider-news-next'),
+            prevEl: $('.js-slider-news-prev')
         },
+
         pagination: {
-            el: ".js-slider-news-pagination",
-            type: "bullets",
-            clickable: !0,
-            renderBullet: function(e, s) {
-                return '<span class="' + s + '">' + (e + 1) + "</span>"
+            el: '.js-slider-news-pagination',
+            type: 'bullets',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + '</span>';
             }
         },
+
         breakpoints: {
             767: {
                 threshold: 0,
@@ -147,116 +170,229 @@ var userAgent = navigator.userAgent.toLowerCase(),
                 slidesPerColumn: 2,
                 slidesPerGroup: 1,
                 spaceBetween: 20,
-                autoHeight: !1
+                autoHeight: false,
             },
             1023: {
                 spaceBetween: 50,
-                slidesPerView: 2
+                slidesPerView: 2,
             },
             1419: {
                 spaceBetween: 20,
-                slidesPerView: 2
+                slidesPerView: 2,
             },
             1919: {
                 spaceBetween: 50,
-                slidesPerView: 2
+                slidesPerView: 2,
             }
         }
-    }), new Swiper(".js-slider-vacancies", {
-        loop: !1,
+    });
+
+    //Slider Vacancies
+    var sliderVacancies = new Swiper('.js-slider-vacancies', {
+        loop: false,
         slidesPerView: 2,
-        spaceBetween: 8.285714285714286 * $(".js-slider-vacancies .swiper-wrapper").width() / 100,
+        spaceBetween: ( $('.js-slider-vacancies .swiper-wrapper').width()*8.285714285714286/100 ), // 116 /1498 full w
         speed: 600,
         threshold: 40,
-        autoHeight: !0,
+        autoHeight: true,
+
         navigation: {
-            nextEl: $(".js-slider-vacancies-next"),
-            prevEl: $(".js-slider-vacancies-prev")
+            nextEl: $('.js-slider-vacancies-next'),
+            prevEl: $('.js-slider-vacancies-prev')
         },
+
         breakpoints: {
             767: {
                 threshold: 0,
                 slidesPerView: 1,
                 slidesPerColumn: 2,
                 spaceBetween: 30,
-                autoHeight: !1
+                autoHeight: false,
             },
             1023: {
                 spaceBetween: 20,
-                slidesPerView: 2
+                slidesPerView: 2,
             },
             1419: {
                 spaceBetween: 24,
-                slidesPerView: 2
+                slidesPerView: 2,
             },
             1919: {
                 spaceBetween: 48,
-                slidesPerView: 2
+                slidesPerView: 2,
             }
         }
     });
-    $(".js-anchor-link").on("click", function(e) {
-        e.preventDefault(), c($(this).attr("href"), 0, 1e3)
+    
+    
+    //Anchor link
+    $('.js-anchor-link').on('click', function(e) {
+        e.preventDefault();
+
+        anchorScroll($(this).attr('href'),0,1000);
     });
-    var c = function(e, s, t) {
-        var a = +$(e).offset().top - s;
-        $("body, html").animate({
-            scrollTop: a
-        }, t)
+
+    var anchorScroll = function(elName,offset,time) {
+        var targetOffset = +$(elName).offset().top - offset;
+
+        $('body, html').animate({scrollTop: targetOffset}, time);
     };
-    $(".js-gallery-item").fancybox({
-        toolbar: !1,
-        infobar: !1,
-        loop: !0,
-        selector: '[data-fancybox*="gallery"]',
+
+
+    //Gallery
+    $('.js-gallery-item').fancybox({
+        toolbar: false,
+        infobar: false,
+        loop: true,
+        selector : '[data-fancybox*="gallery"]',
+
         animationEffect: "zoom",
         transitionEffect: "circular",
         transitionDuration: 600,
-        smallBtn: !0,
+        
+        smallBtn: true,
+        
         image: {
-            preload: !0
+            preload: true
         },
+
         btnTpl: {
-            arrowLeft: '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left"><span class="icon icon-arrow-left"></span></button>',
-            arrowRight: '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right"><span class="icon icon-arrow-right"></span></button>',
-            smallBtn: '<button type="button" data-fancybox-close class="fancybox-button fancybox-close-small"><span class="icon icon-close"></span></button>'
-        }
-    }), $(".js-gallery-more").on("click", function(e) {
+            // Arrows
+            arrowLeft:
+            '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left">' +
+            '<span class="icon icon-arrow-left"></span>' +
+            "</button>",
+
+            arrowRight:
+            '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right">' +
+            '<span class="icon icon-arrow-right"></span>' +
+            "</button>",
+
+            // This small close button will be appended to your html/inline/ajax content by default,
+            // if "smallBtn" option is not set to false
+            smallBtn:
+            '<button type="button" data-fancybox-close class="fancybox-button fancybox-close-small">' +
+            '<span class="icon icon-close"></span>' +
+            "</button>"
+        },
+    });
+
+    ////Gallery more
+    $('.js-gallery-more').on('click', function (e) {
         e.preventDefault();
-        var s = +$(this).data("start"),
-            t = $(this).closest(".js-gallery").find(".js-gallery-item");
-        $.fancybox.open(t, {
-            toolbar: !1,
-            infobar: !1,
-            loop: !0,
-            selector: '[data-fancybox*="gallery"]',
+        var start = +$(this).data('start');
+
+        var $links = $(this).closest('.js-gallery').find('.js-gallery-item');
+
+        $.fancybox.open( $links, {
+            toolbar: false,
+            infobar: false,
+            loop: true,
+            selector : '[data-fancybox*="gallery"]',
+
             animationEffect: "zoom",
             transitionEffect: "circular",
             transitionDuration: 600,
-            smallBtn: !0,
+
+            smallBtn: true,
+
             image: {
-                preload: !0
+                preload: true
             },
+
             btnTpl: {
-                arrowLeft: '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left"><span class="icon icon-arrow-left"></span></button>',
-                arrowRight: '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right"><span class="icon icon-arrow-right"></span></button>',
-                smallBtn: '<button type="button" data-fancybox-close class="fancybox-button fancybox-close-small"><span class="icon icon-close"></span></button>'
+                // Arrows
+                arrowLeft:
+                '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left">' +
+                '<span class="icon icon-arrow-left"></span>' +
+                "</button>",
+
+                arrowRight:
+                '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right">' +
+                '<span class="icon icon-arrow-right"></span>' +
+                "</button>",
+
+                // This small close button will be appended to your html/inline/ajax content by default,
+                // if "smallBtn" option is not set to false
+                smallBtn:
+                '<button type="button" data-fancybox-close class="fancybox-button fancybox-close-small">' +
+                '<span class="icon icon-close"></span>' +
+                "</button>"
+            },
+        }, start );
+    });
+    
+    
+    //Service
+    $('.js-service-toggle').on('click', function(e) {
+        e.preventDefault();
+        $(this).closest('.js-service').toggleClass('open');
+    });
+    
+
+    //Feedback
+    $('.js-feedback-link').on('click', function(e) {
+        e.preventDefault();
+        
+        if ($(window).width() < 768) {
+            $('.js-feedback-circle').addClass('invisible');
+            $('.js-feedback-form').removeClass('open');
+        }
+        
+        $($(this).attr('href')).addClass('open');
+    });
+
+    $('.js-feedback-close').on('click', function(e) {
+        e.preventDefault();
+        $(this).closest('.js-feedback-form').removeClass('open');
+        $(this).closest('.js-feedback-success').removeClass('open');
+        $('.js-feedback-circle').removeClass('invisible');
+    });
+
+    jQuery(document).click(function (e) {
+        var isFeedback = $(e.target).hasClass('js-feedback') || $(e.target).closest('.feedback').hasClass('js-feedback');
+
+        if (!isFeedback) {
+            $('.js-feedback-form').removeClass('open');
+            $('.js-feedback-circle').removeClass('invisible');
+        }
+    });
+    
+    //News
+    $('.js-news-link').on('click',function (e) {
+        e.preventDefault();
+        var popupSrc = $(this).attr('href');
+
+        $.fancybox.open({
+            src  : popupSrc,
+            type : 'inline',
+            opts : {
+                touch: false,
+                animationEffect: "fade",
+                animationDuration: 600,
+                toolbar: false,
+                smallBtn: false,
+                autoFocus: true,
             }
-        }, s)
-    }), $(".js-service-toggle").on("click", function(e) {
-        e.preventDefault(), $(this).closest(".js-service").toggleClass("open")
-    }), $(".js-feedback-link").on("click", function(e) {
-        e.preventDefault(), $(window).width() < 768 && ($(".js-feedback-circle").addClass("invisible"), $(".js-feedback-form").removeClass("open")), $($(this).attr("href")).addClass("open")
-    }), $(".js-feedback-close").on("click", function(e) {
-        e.preventDefault(), $(this).closest(".js-feedback-form").removeClass("open"), $(this).closest(".js-feedback-success").removeClass("open"), $(".js-feedback-circle").removeClass("invisible")
-    }), jQuery(document).click(function(e) {
-        var s = $(e.target).hasClass("js-feedback") || $(e.target).closest(".feedback").hasClass("js-feedback");
-        s || ($(".js-feedback-form").removeClass("open"), $(".js-feedback-circle").removeClass("invisible"))
-    }), $(".js-phone-mask").inputmask({
-        mask: "+7 (999) 999-99-99",
-        showMaskOnHover: !1
-    }), $(".js-date-mask").inputmask({
-        mask: "99.99.99",
-        showMaskOnHover: !1
-    })
+        });
+    });
+
+    $('.js-news-close').on('click', function (e) {
+        e.preventDefault();
+        $.fancybox.close();
+    });
+    
+    
+    //Inpumask
+    $('.js-phone-mask').inputmask({
+        mask: '+7 (999) 999-99-99',
+        showMaskOnHover: false
+    });
+
+    $('.js-date-mask').inputmask({
+        mask: '99.99.99',
+        showMaskOnHover: false
+    });
+    
 });
+
